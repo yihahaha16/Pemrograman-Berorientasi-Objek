bddb320c8c15b40189def330457c09121e82a4fd
yihahaha16 <falihaimtinan@gmail.com>
2026-02-05 14:19:08 +0700

    KodePertama.java

---
+++ Pertemuan1/KodePertama.java
@@ -0,0 +1,11 @@
+package Pertemuan1;
+/*Nama: Faliha Imtinan
+NIM: D1041241004
+Hari/Tanggal: Kami, 5 Januari 2025
+*/
+public class KodePertama{
+    public static void main(String[] args){
+        System.out.println("Halo, Pemrograman Berorientasi Objek!");
+        System.out.println("Ini adalah program Java pertama saya");
+    }
+}
