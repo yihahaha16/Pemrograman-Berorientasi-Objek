5588ef9568a8af5d8aff275e4808436f38d23749
yihahaha16 <falihaimtinan@gmail.com>
2026-02-13 08:20:57 +0700

    p041NamingCovention
    Contoh program p041NamingCovention

---
+++ Pertemuan4/p041NamingConvention.java
@@ -0,0 +1,15 @@
+package Pertemuan4;
+
+public class p041NamingConvention{
+    public static final double PI = 3.14;
+    public static void main(String[] args) {
+        int studentAge = 20;
+        String studentName = "Budi";
+        boolean isActive = true;
+
+        System.out.println("Nama:  " + studentName);
+        System.out.println("Umur:  " + studentAge);
+        System.out.println("Status:  " + isActive);
+        System.out.println("Konstanta PI: " + PI);
+    }
+}
